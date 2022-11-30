82b530c80744860c35a4c58701c845a12c29a0e9
CristianMld <hkrcrm@gmail.com>
2022-11-29 22:12:51 -0300

    purchases

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,9 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getPurchasesThunk } from "../store/slices/purchases.slice";
 
 
 const Purchases = () => {
+
+  const dispatch = useDispatch();
+
+  const purchases = useSelector(state => state.purchases);
+
+  useEffect(() => {
+    dispatch(getPurchasesThunk())
+  }, [])
+
   return (
     <div>
       <h1>Purchases</h1>
+      {
+        purchases.map(purchase => (
+          <li key={purchase.id}>
+            <u>Date:</u> {purchase.cart.createdAt}
+            {purchase.cart.products.map(item => (
+              <Link key={item.id} to={`/product/${item.id}`}>
+                <p>
+                  {item.title}
+                </p>
+              </Link>
+            ))}
+          </li>
+        ))
+      }
     </div>
   );
 };
